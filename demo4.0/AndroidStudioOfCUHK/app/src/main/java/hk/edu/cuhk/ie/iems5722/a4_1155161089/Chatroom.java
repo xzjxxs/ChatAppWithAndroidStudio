9dc0d5f853946a30c2702bf71da740be22853169
RodriguezXu <49096696+xzjxxs@users.noreply.github.com>
2021-12-10 22:46:02 +0800

    demo4.0 -Implement push messages and asynchronous
    Implement push messages in Android using Google Cloud Messaging and use asynchronous tasks and message queues on the server side

---
+++ demo4.0/AndroidStudioOfCUHK/app/src/main/java/hk/edu/cuhk/ie/iems5722/a4_1155161089/Chatroom.java
@@ -0,0 +1,22 @@
+package hk.edu.cuhk.ie.iems5722.a4_1155161089;
+
+import java.io.Serializable;
+
+public class Chatroom implements Serializable {
+    private String id;
+    private String name;
+
+    public Chatroom(String id, String name) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
